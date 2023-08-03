13282173a3ba6b6f635322bf89993b85c9ad2fea
Arpandev Bhattacharya <69894768+Arpandev-code@users.noreply.github.com>
2023-08-03 14:43:49 +0000

    datatypes_summery

---
+++ 01_basics/05_datatypes_summery.js
@@ -0,0 +1,33 @@
+//Primitive:
+// 7 types: String, Number,Boolean, null, undefined,Symbol, Bigint
+
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp= null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId= Symbol('123')
+
+console.log(id === anotherId); //false
+const bigNumber=3425254354353433n
+
+
+
+//Reference(Non primitive)
+
+//Arrray, Objects, Funstions
+//Array
+const heros=["shaktiman","naagraj","doga"]
+
+//object
+let myObj={
+    name:"hitesh",
+    age:22,
+}
+//funstions
+const myFunction=function(){
+    console.log("hello world");
+}
+
