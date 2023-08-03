7ce22141f1e44460c317ed4ef9ae3ef382ec88a8
Arpandev Bhattacharya <69894768+Arpandev-code@users.noreply.github.com>
2023-08-03 08:59:29 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3) // we are using nodejs , not browser
+
+//code readability should be high;
+let name='Arpandev'
+let age=18
+let isLoggedIn=false
+
+//number > 2 to power 53
+//bigint
+//String => ""
+//boolean => true/false
+//null => standalone value
+//undefinded =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof "Hi")
+console.log(typeof null) //Object
+console.log(typeof undefined); //undefined
